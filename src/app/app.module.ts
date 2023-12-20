cb76d5b5ac98cdc276972b7510a867d1b47b0172
Abel Borit <abelboritguitton@gmail.com>
2023-12-20 10:39:37 -0500

    [FEAT] hero-list.component create with comand: ng g component heroes/heroList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
 import { HeroInfoComponent } from './heroes/hero-info/hero-info.component';
+import { HeroListComponent } from './heroes/hero-list/hero-list.component';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent, HeroInfoComponent],
+  declarations: [AppComponent, CounterComponent, HeroInfoComponent, HeroListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/heroes/hero-list/hero-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero-list',
+  templateUrl: './hero-list.component.html',
+  styleUrls: ['./hero-list.component.css'],
+})
+export class HeroListComponent {}
