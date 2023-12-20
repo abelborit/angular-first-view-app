67e5e3732f29dbc5d958b2a71a18d886121a6e59
Abel Borit <abelboritguitton@gmail.com>
2023-12-20 10:28:54 -0500

    [FEAT] hero-info.component create with comand: ng g component heros/heroInfo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { HeroInfoComponent } from './heros/hero-info/hero-info.component';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent],
+  declarations: [AppComponent, CounterComponent, HeroInfoComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
