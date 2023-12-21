a7f4203a5b50def2e9ddc81a199629464a9e4cb7
Abel Borit <abelboritguitton@gmail.com>
2023-12-21 13:22:24 -0500

    [FEAT] dbz.module add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CounterModule, HeroesModule], // colocar o declarar los módulos que creemos
+  imports: [BrowserModule, CounterModule, HeroesModule, DbzModule], // colocar o declarar los módulos que creemos
   providers: [],
   bootstrap: [AppComponent], // nos está indicando que el componente AppComponent será lanzado al iniciar la aplicación
 })
-export class AppModule {}
+export class AppModule { }
